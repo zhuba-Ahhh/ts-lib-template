6fdb71fc330d9b386d8b852366d60e1d5498ca72
zhuba-Ahhh <84793349+zhuba-Ahhh@users.noreply.github.com>
2024-04-12 02:32:28 +0000

    Deploying to gh-pages from @ zhuba-Ahhh/ts-lib-template@00acaa367819b9e66456de6657d0897a2933f687 🚀

---
+++ index.mjs
@@ -0,0 +1,12 @@
+// src/index.ts
+if (false) {
+  console.log("Now it's development mode");
+} else if (true) {
+  console.log("Now it's production mode");
+}
+var src_default = () => {
+  console.log("Hello World!");
+};
+export {
+  src_default as default
+};
